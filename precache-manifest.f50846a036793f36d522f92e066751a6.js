83c5358af0684e5f8f576f0f7183e648287ba8d7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-04 15:21:30 +0000

    Deploy blinkcat/rmovies to github.com/blinkcat/rmovies.git:gh-pages

---
+++ precache-manifest.f50846a036793f36d522f92e066751a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "801a20c826bafe2240447064067ee8f1",
+    "url": "/rmovies/index.html"
+  },
+  {
+    "revision": "808ac6403cfe9169712d",
+    "url": "/rmovies/static/css/main.6eaee5e0.chunk.css"
+  },
+  {
+    "revision": "4743a297b3410c4fd3e0",
+    "url": "/rmovies/static/js/2.e42c8c4c.chunk.js"
+  },
+  {
+    "revision": "283e247fcd70152d45e39e6e5a17cbed",
+    "url": "/rmovies/static/js/2.e42c8c4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "808ac6403cfe9169712d",
+    "url": "/rmovies/static/js/main.14206d63.chunk.js"
+  },
+  {
+    "revision": "46ec81fa7a7522c3965b",
+    "url": "/rmovies/static/js/runtime-main.a156d0cb.js"
+  }
+]);
